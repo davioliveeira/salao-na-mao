455680e6fc63680092906ac5a7612d04c535b541
davi.silva.3.glo <davi.silva.3@globalhitss.com.br>
2023-05-06 15:41:27 -0300

    Adding Route Horario Not Completed

--- ws/src/routes/horarios.routes.js
+++ ws/src/routes/horarios.routes.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+const Horario = require('../models/horario.js');
 
 router.post('/', async (req, res) => {
     try {
-        
+        const horarios = await Horario( req.body ).save();
+        res.status(200).json( {message : 'Horarios criados com Sucesso!', horarios : horarios});
     } catch (err) {
         res.json({ error: true, message : err.message });
     }
 
 })
+
+
+module.exports = router;
