8990b472040b9d6c2ffa66aec6af1b1a36a622e0
davi.silva.3.glo <davi.silva.3@globalhitss.com.br>
2023-05-06 05:29:06 -0300

    Finish Routes of Salao

--- ws/src/routes/salao.routes.js
+++ ws/src/routes/salao.routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router();
 const Salao = require('../models/salao.js');
 const Servicos = require('../models/services.js');
+const turf = require('turf'); 
+
 
 router.post('/', async (req, res) => {
     try {
@@ -13,7 +15,7 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.get('/:salaoId', async (req, res) => {
+router.get('/servicos/:salaoId', async (req, res) => {
     try {
         const { salaoId } = req.params;
         const servicos = await Servicos.find({
@@ -28,5 +30,21 @@ router.get('/:salaoId', async (req, res) => {
         res.json({ error: true, message : err.message });
     }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const salao = await Salao.findById(req.params.id).select(
+            'capa nome enderco.cidade geo.coordinates telefone'
+        );
+        // Calculando a distancia :
+        const distance = turf.distance(turf.point(salao.geo.coordinates),turf.point([-3.8276774, -38.5146807]))
+
+
+        res.status(200).json({ message : `Essas são as informações desse Salão : ${salao}` , distancia : distance });
+    }catch (err) {
+        res.json({ error: true, message : err.message });
+    }
+});
+
 
 module.exports = router ; 
